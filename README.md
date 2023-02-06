d967852a798792806783417608b272cb603be21e
allesoares95 <allesoares_@live.com>
2023-02-06 10:04:51 -0300

    serve.json add | creteAPI

--- README.md
+++ README.md
@@ -28,5 +28,29 @@ Dialog.Close
 
 Radio Group
 npm install @radix-ui/react-radio-group
-```
 
+JSON Server 
+https://github.com/rocketseat-education/ignite-reactjs-03-dt-money/commit/d6085ce590508a543af916856c23d33e68019b9d
+
+npm i json-server -D
+
+para rodar json 
+npx json-server server.json
+
+para deixar atualizando json automatico utilizar -w no final 
+npx json-server server.json -w
+
+adicionando delay nas requisições de 500ms
+npx json-server server.json -w -d 500
+
+consultando datas no console web
+new Date().toISOString()
+
+pode alterar npm run pelo package.json
+adicionando um novo scripts 
+"dev:server": "json-server server.json -w -d 500"
+
+rodar:
+npm run dev:server
+
+```
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Sumarry } from "../../components/Sumarry";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 
+interface Transaction {
+  id: number;
+  description: string;
+  type: 'income' | 'outcome';
+  price: number;
+  category: string;
+  createAt: string;
+}
+
 export function Transactions() {
+  const [transactions, setTransaction] = useState<Transaction[]>([])
+
+  async function loadTransactions() {
+    const response = await fetch('http://localhost:3000/transactions')
+    const data = await response.json();
+
+    setTransaction(data);
+  }
+
+  useEffect(() => {
+    loadTransactions();
+  }, [])
+
   return (
     <div>
       <Header />
@@ -14,82 +37,20 @@ export function Transactions() {
 
         <TransactionsTable>
           <tbody>
-            <tr>
-              <td width="50%">Desenvolvimento de site</td>
-              <td>
-                <PriceHighLight variant="income">
-                  R$ 12.000,00
-                </PriceHighLight>
-              </td>
-              <td>Venda</td>
-              <td>31/01/2023</td>
-            </tr>
-
-            <tr>
-              <td width="50%">Aluguel do apartamento</td>
-              <td>
-              <PriceHighLight variant="outcome">
-                - R$ 1.200,00
-              </PriceHighLight>
-              </td>
-              <td>Casa</td>
-              <td>20/01/2023</td>
-            </tr>
-
-            {/* <tr>
-              <td width="50%">Computador</td>
-              <td>
-                <PriceHighLight variant="income">
-                R$ 5.400,00
-                </PriceHighLight>
-              </td>
-              <td>Venda</td>
-              <td>10/01/2023</td>
-            </tr>
-
-            <tr>
-              <td width="50%">Desenvolvimento de site</td>
-              <td>
-                <PriceHighLight variant="income">
-                R$ 8.000,00
-                </PriceHighLight>
-              </td>
-              <td>Venda</td>
-              <td>08/01/2023</td>
-            </tr>
-
-            <tr>
-              <td width="50%">Fone de ouvido</td>
-              <td>
-              <PriceHighLight variant="outcome">
-              - R$ 150,00
-              </PriceHighLight>
-              </td>
-              <td>Itens</td>
-              <td>60/01/2023</td>
-            </tr>
-
-            <tr>
-              <td width="50%">Despesa mensal</td>
-              <td>
-              <PriceHighLight variant="outcome">
-                - R$ 1.500,00
-              </PriceHighLight>
-              </td>
-              <td>Alimentação</td>
-              <td>05/01/2023</td>
-            </tr>
-
-            <tr>
-              <td width="50%">Desenvolvimento de site</td>
-              <td>
-                <PriceHighLight variant="income">
-                  R$ 3.000,00
-                </PriceHighLight>
-              </td>
-              <td>Venda</td>
-              <td>03/01/2023</td>
-            </tr> */}
+            {transactions.map(transactions => {
+              return (
+                <tr key={transactions.id}>
+                  <td width="50%">{transactions.description}</td>
+                  <td>
+                    <PriceHighLight variant={transactions.type}>
+                      {transactions.price}
+                    </PriceHighLight>
+                  </td>
+                  <td>{transactions.category}</td>
+                  <td>{transactions.createAt}</td>
+                </tr>
+              )
+            })}
 
           </tbody>
         </TransactionsTable>
